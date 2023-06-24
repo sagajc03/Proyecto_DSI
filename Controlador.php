fbaa5bd72f2bd0f674f878afc82f9c97b3981771
Juan Salas <sagajc03@gmail.com>
2023-06-24 00:07:30 -0600

    Inicio de sesion

--- Controlador.php
+++ Controlador.php
@@ -1,13 +1,13 @@
 <?php
     function Conectar(){
-        $Server="124.0.0.1";
+        $Server="127.0.0.1";
         $User="root";
         $Pws="";
         $BD="comprobante_fiscal";
         $Con=mysqli_connect($Server,$User,$Pws,$BD);
         return $Con;
     }
-    
+
     function Ejecutar($Con,$SQL){
         $Result=mysqli_query($Con,$SQL) or die(mysqli_error($Con));
         return $Result;
